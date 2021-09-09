c4ecf56d499f51044624370e67a7dfd0608db997
rent4yj <rent4yj@gmail.com>
2021-09-09 20:17:14 +0900

    9월9일

---
+++ chapter_2_3.py
@@ -0,0 +1,14 @@
+string = input("안녕하세요")
+print (string)
+print(type(string))
+int_a = int(string)
+number = input("숫자를 입력하세요")
+print(number)
+print(type(number))
+
+int_b = int(number)
+
+print("문자열 자료 : ",string+number)
+print("숫자 자료 : ",int_a+int_b)
+
+
---
+++ chapter_2_4.py
@@ -0,0 +1,102 @@
+string_a = "{}".format(10)
+
+print(string_a)
+print(type(string_a))
+
+string_a = "{} 만원".format(5000)
+string_b = "파이썬 열공하여 첫 연봉 {}만 원 만들기".format(5000)
+string_c = "{}{}{}".format(3000 , 4000 , 5000 )
+string_d = "{}{}{}".format(1,"문자열",True)
+
+
+print(string_a)
+print(string_b)
+print(string_c)
+print(string_d)
+
+string_a = "{}{}{}".format(5000,1,2,3,4)
+#string_b = "{}{}{}".format(5000)
+
+print(string_a)
+print(string_b)
+
+
+output_a = "{:d}".format(52)
+output_b = "{:5d}".format(52)
+output_c = "{:10d}".format(52)
+output_d = "{:05d}".format(52)
+output_e = "{:05d}".format(-52)
+print("기본")
+print(output_a)
+print("특정칸에 출력")
+print(output_b)
+print(output_c)
+print("빈칸에 0 체우기")
+print(output_d)
+print(output_e)
+
+output_a = "{:+d}".format(52)
+output_b = "{:+d}".format(-52)
+output_c = "{:d}".format(52)
+output_d = "{:d}".format(-52)
+print("기호와 함께 출력하기")
+print(output_a)
+print(output_b)
+print(output_c)
+print(output_d)
+
+output_a = "{:+5d}".format(52)
+output_b = "{:+5d}".format(-52)
+output_c = "{:=+5d}".format(52)
+output_d = "{:=+5d}".format(-52)
+output_e = "{:+05d}".format(52)
+output_f = "{:+05d}".format(-52)
+
+print("조합")
+print(output_a)
+print(output_b)
+print(output_c)
+print(output_d)
+print(output_e)
+print(output_f)
+
+output_a = "{:f}".format(52.275)
+output_b = "{:15f}".format(52.275)
+output_c = "{:+15f}".format(52.275)
+output_d = "{:+015f}".format(52.275)
+print("부동소숫점 출력")
+print(output_a)
+print(output_b)
+print(output_c)
+print(output_d)
+
+output_a = "{:15.3f}".format(52.275)
+output_b = "{:15.2f}".format(52.275)
+output_c = "{:15.1f}".format(52.275)
+print("부동소숫점 출력")
+print(output_a)
+print(output_b)
+print(output_c)
+
+output_a = 52.0
+output_b = "{:g}".format(output_a)
+print("의미없는 소숫점 지우기")
+print(output_a)
+print(output_b)
+
+print("대소문자 변환")
+
+a = "Hello Python Programming...!"
+b = a.upper()
+print(b)
+b = a.lower()
+print(b)
+
+a = """
+    안녕하세요    
+문자열의 함수를 알아봅니다.
+"""
+print(a)
+print(a.strip())
+print(a.lstrip())
+print(a.rsplit())
